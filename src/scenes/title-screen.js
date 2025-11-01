0b94eb16ebd56b0e36f7af9d502893e5adb291f4
Pazneria <j.moren.awesome@gmail.com>
2025-11-01 19:03:40 -0400

    Scale game viewport to full screen

--- src/scenes/title-screen.js
+++ src/scenes/title-screen.js
@@ -8,11 +8,16 @@ export class TitleScreen {
   }
 
   #createView() {
+    const wrapper = document.createElement('div');
+    wrapper.className = 'title-screen scene';
+
     const image = document.createElement('img');
     image.className = 'title-screen__image';
     image.src = 'assets/images/sword-guys-title.png';
     image.alt = 'Sword Guys title screen';
 
-    return image;
+    wrapper.append(image);
+
+    return wrapper;
   }
 }
